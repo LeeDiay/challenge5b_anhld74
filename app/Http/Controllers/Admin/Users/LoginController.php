3c6bfd9e27d8a1cf128c33e8b86faa589b815552
LeeDiay <leducanh1503.works@gmail.com>
2024-03-15 11:43:59 +0700

    start project

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('admin.users.login', [
+            'title' => 'Dang nhap'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'email' => 'required|max:255|email:filter',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ], $request->input('remember'))) {
+            return redirect()->route('admin');
+        }
+
+        session()->flash('error', 'Email hoac password khong dung');
+        return redirect()->back();
+    }
+}
